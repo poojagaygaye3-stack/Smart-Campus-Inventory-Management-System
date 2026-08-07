90fb688d143d402a170175f8864109081d4f6d2d
Pooja Gaygaye <poojagaygaye3@gmail.com>
2026-08-07 22:45:29 +0530

    Create admin_header.php

---
+++ admin_header.php
@@ -0,0 +1,22 @@
+<!-- header.php -->
+<link rel="stylesheet" href="qheader.css">
+
+<header>
+    <div class="logo">
+        <img src="logo.png" alt="Logo">
+        <h1>Smart Campus Inventory</h1>
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="admin_home.php" >Home</a></li>
+            <li><a href="admin_add.php">Add Item</a></li>
+            <li><a href="admin_report.php">Print</a></li>
+            <li><a href="admin_barcode.php">Barcode</a></li>
+            <li><a href="admin_qr.php" class="active">QRcode</a></li>
+             
+        </ul>
+    </nav>
+</header>
+
+
