1079c0fc00c87020b4711af4c3045c6d6f205f04
Pooja Gaygaye <poojagaygaye3@gmail.com>
2026-08-10 15:21:31 +0530

    Create faculty_header.php

---
+++ faculty_header.php
@@ -0,0 +1,22 @@
+<!-- header.php -->
+<link rel="stylesheet" href="qheader.css">
+
+<header>
+    <div class="logo">
+        <img src="logo.png" alt="Logo">
+        <h1>Smart Campus Inventory</h1>
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="faculty_office.php" >Home</a></li>
+            <li><a href="faculty_add.php">Add Item</a></li>
+            <li><a href="faculty_report.php">Print</a></li>
+            <li><a href="faculty_barcode.php">Barcode</a></li>
+            <li><a href="faculty_qr.php" class="active">QRcode</a></li>
+             
+        </ul>
+    </nav>
+</header>
+
+
