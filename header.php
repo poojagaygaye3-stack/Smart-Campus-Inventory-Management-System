dcaadf87239b2702f7491629107be30a7d397d8d
Pooja Gaygaye <poojagaygaye3@gmail.com>
2026-08-15 18:31:36 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>College Inventory</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+
+<header>
+    <div class="logo">
+        <img src="logo.png" alt="Logo">
+        <h1>Smart Campus Inventory</h1>
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="#" class="active">Inventory</a></li>
+            <li><a href="add.php">Add Item</a></li>
+            <li><a href="#">Update Item</a></li>
+            <li><a href="report.php">Reports</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+    </nav>
+</header>
+<footer id="footer">
+    <center>
+        <h1>Smart Campus Inventory</h1>
+        <p>Maharashtra, Pune, Koregaon Park 411001</p>
+        <p>+91-123-456-7890</p>
+
+        <div class="social-icons">
+            <img src="https://cdn-icons-png.flaticon.com/256/2504/2504839.png" width="40">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Facebook_Logo_2023.png/768px-Facebook_Logo_2023.png" width="40">
+            <img src="https://i2.wp.com/freepngimg.com/download/logo/69732-logo-facebook-instagram-icone-free-png-hq.png" width="40">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" width="40">
+        </div>
+
+        <div>&copy; <strong>Smart Campus Inventory</strong>. All Rights Reserved</div>
+        <a href="#" style="color: #fff;">Designed by POOJA</a>
+    </center>
+</footer>
